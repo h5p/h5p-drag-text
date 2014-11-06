9e94bdcc30359f93f6ad26d0bc9029957ce3c7df
Thomas <thomasmars87@gmail.com>
2014-11-06 17:25:24 +0100

    Added css for tip container and feedback styling. Restructured a lot of the JS code and added functionality. Added more customizable semantics.

--- js/dragText.js
+++ js/dragText.js
@@ -10,6 +10,25 @@ H5P.DragText = (function ($) {
   var INNER_CONTAINER = "h5p-word-inner";
   var TITLE_CONTAINER = "h5p-word-title";
   var WORDS_CONTAINER = "h5p-word-selectable-words";
+  var FOOTER_CONTAINER = "h5p-word-footer";
+  var EVALUATION_CONTAINER = "h5p-word-evaluation-container";
+  var BUTTON_CONTAINER = "h5p-button-bar";
+  var DROPZONE_CONTAINER = "h5p-dropzone-container";
+
+  //Special Sub-containers:
+  var EVALUATION_SCORE = "h5p-word-evaluation-score";
+  var EVALUATION_EMOTICON = "h5p-word-evaluation-score-emoticon";
+  var EVALUATION_EMOTICON_MAX_SCORE = "max-score";
+  var DROPZONE = "h5p-dropzone";
+
+  //CSS Buttons:
+  var BUTTONS = "h5p-button";
+  var CHECK_BUTTON = "h5p-check-button";
+  var RETRY_BUTTON = "h5p-retry-button";
+
+  //CSS Dropzone feedback:
+  var CORRECT_FEEDBACK = 'h5p-correct-feedback';
+  var WRONG_FEEDBACK = 'h5p-wrong-feedback';
 
   /**
    * Initialize module.
@@ -28,8 +47,13 @@ H5P.DragText = (function ($) {
       textField: "This is a *nice*, *flexible* content type, which allows you to highlight all the *wonderful* words in this *exciting* sentence.\n"+
         "This is another line of *fantastic* text.",
       checkAnswer: "Check",
+      enableCheckAnswer: true,
       tryAgain: "Retry",
-      score: "Score : @score of @total, correct: @correct, wrong: @wrong, missed: @missed."
+      enableTryAgain: true,
+      score: "Score : @score of @total.",
+      showSolution : "Show Solution",
+      enableShowSolution: true,
+      instantFeedback: false
     }, params);
   }
 
@@ -42,6 +66,118 @@ H5P.DragText = (function ($) {
         .html('<div class=' + INNER_CONTAINER + '><div class=' + TITLE_CONTAINER + '>' + this.params.taskDescription + '</div></div>')
         .children();
     this.addTaskTo(this.$inner);
+
+    // Add score and button containers.
+    this.addFooter();
+  };
+
+  /**
+   * Append footer to inner block.
+   */
+  C.prototype.addFooter = function () {
+    this.$footer = $('<div class=' + FOOTER_CONTAINER + '></div>').appendTo(this.$inner);
+    this.$evaluation = $('<div class=' + EVALUATION_CONTAINER + '></div>').appendTo(this.$footer);
+    this.addButtons();
+  };
+
+  /**
+   * Add check solution and retry buttons.
+   */
+  C.prototype.addButtons = function () {
+    var self = this;
+    self.$buttonContainer = $('<div/>', {'class': BUTTON_CONTAINER});
+
+    var $checkAnswerButton = $('<button/>', {
+      class: BUTTONS+' '+CHECK_BUTTON,
+      type: 'button',
+      text: this.params.checkAnswer
+    }).appendTo(self.$buttonContainer).click(function () {
+      self.showEvaluation();
+      if (!self.showEvaluation() && self.params.enableTryAgain) {
+        $retryButton.show();
+      }
+      else {
+        $retryButton.hide();
+        $checkAnswerButton.hide();
+      }
+    });
+
+    var $retryButton =  $('<button/>', {
+      class: BUTTONS+' '+RETRY_BUTTON,
+      type: 'button',
+      text: this.params.tryAgain
+    }).appendTo(self.$buttonContainer).click(function () {
+      self.resetTask();
+      self.hideEvaluation();
+      $retryButton.hide();
+      if (self.params.enableCheckAnswer) {
+        $checkAnswerButton.show();
+      }
+    });
+
+    if (!self.params.enableCheckAnswer) {
+      $checkAnswerButton.hide();
+    }
+
+    self.$buttonContainer.appendTo(self.$footer);
+  };
+
+  /**
+   * Resets the buttons back to their original position.
+   */
+  C.prototype.resetTask = function () {
+    this.draggablesArray.forEach(function (entry) {
+      entry.resetPosition();
+    });
+  };
+
+  /**
+   * Evaluate task and display score text for word markings.
+   *
+   * @return {Boolean} Returns true if maxScore was achieved.
+   */
+  C.prototype.showEvaluation = function () {
+    this.hideEvaluation();
+    this.calculateScore();
+
+    var score = this.correctAnswers;
+    var maxScore = this.droppablesArray.length;
+
+    var scoreText = this.params.score.replace(/@score/g, score.toString())
+      .replace(/@total/g, maxScore.toString());
+
+    //Append evaluation emoticon and score to evaluation container.
+    $('<div class='+EVALUATION_EMOTICON+'></div>').appendTo(this.$evaluation);
+    $('<div class=' + EVALUATION_SCORE + '>' + scoreText + '</div>').appendTo(this.$evaluation);
+
+    if (score === maxScore) {
+      this.$evaluation.addClass(EVALUATION_EMOTICON_MAX_SCORE);
+      this.disableDraggables();
+    }
+    else {
+      this.$evaluation.removeClass(EVALUATION_EMOTICON_MAX_SCORE);
+    }
+    return score === maxScore;
+  };
+
+  /**
+   * Calculate score and store them in class variables.
+   */
+  C.prototype.calculateScore = function () {
+    var self = this;
+    self.correctAnswers = 0;
+    self.droppablesArray.forEach(function (entry) {
+      if(entry.isCorrect()) {
+        self.correctAnswers += 1;
+      }
+    });
+  };
+
+  /**
+   * Clear the evaluation text.
+   */
+  C.prototype.hideEvaluation = function () {
+    this.$evaluation.html('');
   };
   
   /**
@@ -50,7 +186,6 @@ H5P.DragText = (function ($) {
    */
   C.prototype.addTaskTo = function ($container) {
     var self = this;
-    var textField = self.params.textField;
     self.clozeIndex = 0;
     self.clozesArray = [];
     self.droppablesArray = [];
@@ -60,9 +195,19 @@ H5P.DragText = (function ($) {
       class: 'draggables-container'
     });
     self.$wordContainer = $('<div/>', {'class': WORDS_CONTAINER});
+    self.handleText();
 
+    //$wordContainer.html(textField);
+    self.$wordContainer.appendTo($container);
+    self.$draggables.appendTo($container);
+  };
+
+  C.prototype.handleText = function () {
+    var self = this;
+    var textField = self.params.textField;
     // Go through the text and replace all the asterisks with input fields
-    var clozeEnd, clozeStart = textField.indexOf('*');
+    var clozeStart = textField.indexOf('*');
+    var clozeEnd = textField.indexOf('*');
     var currentIndex = 0;
     while (clozeStart !== -1 && clozeEnd !== -1) {
       clozeStart++;
@@ -72,28 +217,21 @@ H5P.DragText = (function ($) {
       }
       // Create new cloze
       self.clozeIndex++;
-      self.addCloze(textField.substring(clozeStart, clozeEnd));
-
-      clozeEnd++;
-
       self.$wordContainer.append(textField.slice(currentIndex, clozeStart - 1));
+      self.addCloze(textField.substring(clozeStart, clozeEnd));
+      clozeEnd++;
       currentIndex = clozeEnd;
-
 
       // Find the next cloze
       clozeStart = textField.indexOf('*', clozeEnd);
     }
     self.$wordContainer.append(textField.slice(currentIndex, textField.length-1));
-
-    //$wordContainer.html(textField);
-    self.$wordContainer.appendTo($container);
-    self.$draggables.appendTo($container);
   };
 
-  C.prototype.addCloze = function (answer) {
+  C.prototype.addCloze = function (text) {
     var self = this;
     var tip = undefined;
-    var answer = answer;
+    var answer = text;
     var answersAndTip = answer.split(':');
 
     if(answersAndTip.length > 0) {
@@ -101,98 +239,187 @@ H5P.DragText = (function ($) {
       tip = answersAndTip[1];
     }
 
-    var $dropZone = $('<input/>', {
-      class: 'h5p-dropzone',
-      disabled: true
-    }).droppable({
-      drop: function (event, ui) {
-        self.draggablesArray.forEach( function (entry) {
-          if (entry.getDraggableElement().is(ui.draggable)) {
-            droppable.setDraggable(entry);
-          }
-        });
-      }
-    });
-
-    var $draggableWord = $('<span/>', {
-      html: 'draggable'
-    }).appendTo(self.$draggables)
-      .draggable({
-        revert: "invalid"
-      }
-    );
-
-    var draggable = new Draggable($draggableWord, self.clozeIndex, self.$draggables);
-    var droppable = new Droppable($dropZone, draggable);
+    var draggable = new Draggable(answer, self.clozeIndex);
+    draggable.appendDraggableTo(self.$draggables);
+    var droppable = new Droppable(answer, tip, draggable, self.draggablesArray, self.params.instantFeedback);
+    droppable.appendDroppableTo(self.$wordContainer);
     self.draggablesArray.push(draggable);
     self.droppablesArray.push(droppable);
-
-    self.$wordContainer.append($dropZone);
   };
 
-  function Draggable($draggable, initialPosition, $container) {
+  C.prototype.disableDraggables = function () {
+    this.draggablesArray.forEach( function (entry) {
+      entry.disableDraggable();
+    });
+  };
+
+  function Draggable(text, initialPosition) {
     var self = this;
-    self.isInDropZone = false;
-    self.$container = $container;
-    self.isInCorrectDropZone = false;
+    self.text = text;
     self.initialPosition = initialPosition;
-    self.$draggable = $draggable;
+    self.$draggable = null;
     self.insideDropzone = null;
 
+    self.createDraggable();
   }
+
+  Draggable.prototype.disableDraggable = function () {
+    this.$draggable.draggable({ disabled: true});
+  };
+
+  Draggable.prototype.createDraggable = function () {
+    var self = this;
+    this.$draggable = $('<span/>', {
+      html: this.text
+    }).draggable({
+        revert: function (isValidDrop) {
+          this.data("uiDraggable").originalPosition = {
+            top: 0,
+            left: 0
+          };
+          if (!isValidDrop) {
+            self.removeFromZone();
+            return true;
+          }
+        }
+    });
+  };
+
+  Draggable.prototype.appendDraggableTo = function ($container) {
+    this.$draggable.appendTo($container);
+  };
 
   Draggable.prototype.getDraggableElement = function () {
     return this.$draggable;
   };
 
   Draggable.prototype.resetPosition = function () {
-    //Put this object before the item with higher
-    // initialPosition than this in the remaining draggables.
     this.removeFromZone();
     this.$draggable.css({ left: 0, top:0 });
   };
 
   Draggable.prototype.removeFromZone = function () {
-    this.insideDropzone.removeDraggable();
-    this.isInDropZone = false;
-    this.insideDropzone = null;
+    if (this.insideDropzone !== null) {
+      this.insideDropzone.removeDraggable();
+    }
+  };
 
+  Draggable.prototype.setZone = function (zone) {
+    this.insideDropzone = zone;
   };
 
   Draggable.prototype.addToZone = function (droppable) {
-    if (this.isInDropZone) {
+    if (this.insideDropzone !== null) {
       this.insideDropzone.removeDraggable();
     }
-    this.isInDropZone = true;
     this.insideDropzone = droppable;
+  };
+
+  function Droppable(text, tip, correctDraggable, draggablesArray, instantFeedback) {
+    var self = this;
+    self.correctDraggable = correctDraggable;
+    self.draggablesArray = draggablesArray;
+    self.instantFeedback = instantFeedback;
+    self.text = text;
+    self.tip = tip;
+    self.containedDraggable = null;
+    self.$dropzoneContainer = null;
+
+    self.createDroppable();
   }
 
-  function Droppable($droppable, correctDraggable) {
-    this.correctDraggable = correctDraggable;
-    var isCorrect = false;
-    var hasDraggable = false;
-    this.containedDraggable = null;
-  }
+  Droppable.prototype.createDroppable = function () {
+    var self = this;
+    self.$dropzoneContainer = $('<span/>', {
+      class: DROPZONE_CONTAINER
+    });
 
-  Droppable.prototype.setDraggable = function(droppedDraggable) {
-    if (this.containedDraggable === droppedDraggable) {
-      return;
-    }
-    else if (this.containedDraggable === null) {
-      droppedDraggable.addToZone(this);
-      this.containedDraggable = droppedDraggable;
-    }
-    else {
-      this.containedDraggable.resetPosition();
-      this.containedDraggable = droppedDraggable;
-      droppedDraggable.addToZone(this);
+    $('<input/>', {
+      class: DROPZONE,
+      disabled: true
+    }).droppable({
+      drop: function (event, ui) {
+        self.draggablesArray.forEach( function (entry) {
+          if (entry.getDraggableElement().is(ui.draggable)) {
+            self.setDraggable(entry);
+          }
+        });
+      }
+    }).appendTo(self.$dropzoneContainer);
+
+    if(self.tip !== undefined) {
+      self.$dropzoneContainer.append(H5P.JoubelUI.createTip(self.tip, self.$dropzoneContainer));
     }
   };
 
-  Droppable.prototype.removeDraggable = function () {
-    this.containedDraggable = null;
-  }
+  Droppable.prototype.appendDroppableTo = function ($container) {
+    this.$dropzoneContainer.appendTo($container);
+  };
 
+  Droppable.prototype.setDraggable = function(droppedDraggable) {
+    var self = this;
+    if (self.containedDraggable === droppedDraggable) {
+      return;
+    }
+    if (self.containedDraggable !== null) {
+      self.containedDraggable.resetPosition();
+    }
+    self.containedDraggable = droppedDraggable;
+    droppedDraggable.addToZone(this);
+    if (self.instantFeedback) {
+      self.setFeedback();
+      self.isAllFilled();
+    }
+  };
+
+  Droppable.prototype.isAllFilled = function () {
+    var self = this;
+    var allFilled = true;
+    self.draggablesArray.forEach(function (entry) {
+      if (entry.insideDropzone === null) {
+        allFilled = false;
+        //Searches for the retry button and hides it.
+        self.$dropzoneContainer.parent().parent().find("."+RETRY_BUTTON).hide();
+        return false;
+      }
+    });
+    if (allFilled){
+      //Searches for the retry button and displays it.
+      self.$dropzoneContainer.parent().parent().find("."+RETRY_BUTTON).show();
+    }
+    return true;
+  };
+
+  Droppable.prototype.removeDraggable = function () {
+    if (this.instantFeedback) {
+      this.containedDraggable.setZone(null);
+      this.removeFeedback();
+      this.isAllFilled();
+    }
+    this.containedDraggable = null;
+  };
+
+  Droppable.prototype.isCorrect = function () {
+    if (this.containedDraggable === this.correctDraggable) {
+      return true;
+    }
+  };
+
+  Droppable.prototype.setFeedback = function () {
+    if (this.isCorrect()) {
+      this.$dropzoneContainer.removeClass(WRONG_FEEDBACK);
+      this.$dropzoneContainer.addClass(CORRECT_FEEDBACK);
+    }
+    else {
+      this.$dropzoneContainer.removeClass(CORRECT_FEEDBACK);
+      this.$dropzoneContainer.addClass(WRONG_FEEDBACK);
+    }
+  };
+
+  Droppable.prototype.removeFeedback = function () {
+    this.$dropzoneContainer.removeClass(WRONG_FEEDBACK);
+    this.$dropzoneContainer.removeClass(CORRECT_FEEDBACK);
+  };
 
     return C;
 })(H5P.jQuery);

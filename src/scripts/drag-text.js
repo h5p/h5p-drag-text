1632bcfc2a51c25a791b2c860f716c81effeec95
Lars-Magnus Underhaug <lars-magnus.underhaug@joubel.com>
2022-03-10 10:11:57 +0100

    JI-3338 Truncate draggables according to dropzone

--- src/scripts/drag-text.js
+++ src/scripts/drag-text.js
@@ -159,6 +159,8 @@ H5P.DragText = (function ($, Question, ConfirmationDialog) {
       this.toggleDropEffect();
       element.setAttribute('aria-grabbed', 'true')
       this.setDraggableAriaLabel(draggable);
+      var dropzoneContainerWith = document.querySelector('.' + DROPZONE_CONTAINER).getBoundingClientRect().width;
+      element.style.maxWidth = `${dropzoneContainerWith * 0.4}px`;
     });
 
     this.on('stop', event => {
@@ -169,6 +171,7 @@ H5P.DragText = (function ($, Question, ConfirmationDialog) {
       this.toggleDropEffect();
       element.setAttribute('aria-grabbed', 'false')
       this.setDraggableAriaLabel(draggable);
+      element.style.maxWidth = "100%";
     });
 
     // on drop, remove all dragging

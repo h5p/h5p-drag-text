a9bb8d9bc71460d865d854025950ed921dd20a05
Paal Joergensen <pal.jorgensen@gmail.com>
2023-04-27 12:46:26 +0200

    Update node modules

--- webpack.config.js
+++ webpack.config.js
@@ -2,59 +2,59 @@ const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const nodeEnv = process.env.NODE_ENV || 'development';
-const isProd = (nodeEnv === 'production');
-const libraryName = process.env.npm_package_name;
+module.exports = (env, argv) => {
+  const mode = argv.mode;
+  const isProd = (mode === 'production');
+  const libraryName = process.env.npm_package_name;
 
-const config = {
-  mode: nodeEnv,
-  context: path.resolve(__dirname, 'src/entries'),
-  entry: "./dist.js",
-  devtool: (isProd) ? undefined : 'inline-source-map',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: `${libraryName}.js`
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(svg)$/,
-        include: path.join(__dirname, 'src/images'),
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10000
+  return {
+    mode: mode,
+    context: path.resolve(__dirname, 'src/entries'),
+    entry: "./dist.js",
+    devtool: (isProd) ? undefined : 'inline-source-map',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: `${libraryName}.js`
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.(svg)$/,
+          include: path.join(__dirname, 'src/images'),
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 10000
+              }
             }
-          }
-        ]
-      } // inline base64 URLs for <=10k images, direct URLs for the rest
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: `${libraryName}.css`
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
-    })
-  ],
-  stats: {
-    colors: true
-  }
+          ]
+        } // inline base64 URLs for <=10k images, direct URLs for the rest
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: `${libraryName}.css`
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify(mode)
+        }
+      })
+    ],
+    stats: {
+      colors: true
+    }
+  };
 };
-
-module.exports = config;

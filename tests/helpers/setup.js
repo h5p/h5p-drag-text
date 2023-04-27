04274e5f67595670eec92ab7e62cd5ed0c125fa3
Paal Joergensen <pal.jorgensen@gmail.com>
2023-04-27 12:59:49 +0200

    Fix running ava tests

--- tests/helpers/setup.js
+++ tests/helpers/setup.js
@@ -1,6 +1,2 @@
-require('browser-env')();
 const hooks = require('require-extension-hooks');
-
-// Setup vue files to be processed by `require-extension-hooks-vue`
-// Setup vue and js files to be processed by `require-extension-hooks-babel`
 hooks(['js']).plugin('babel').push();
